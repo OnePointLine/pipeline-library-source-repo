d3470a38bf8ae437ecd7edd02b5ea20f3d91b631
onepointline <onepointline@gmail.com>
2020-04-22 22:26:55 +0300

    first commit. Stab for call() for git library

---
+++ git/sync.groovy
@@ -0,0 +1,8 @@
+
+// https://jenkinsci.github.io/templating-engine-plugin/1.1.1/pages/Templating/configuration_files/default_step_implementation.html
+
+void call(){
+    println "gradle: build()"
+}
+
+
