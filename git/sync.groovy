5d8f1a03ee0fd79a67d97f301c1f3bef7f3191c8
onepointline <onepointline@gmail.com>
2020-04-22 22:33:42 +0300

    Rearrenging a call() method so that it would more like Jenkins pipieline

--- git/sync.groovy
+++ git/sync.groovy
@@ -1,8 +1,11 @@
 
 // https://jenkinsci.github.io/templating-engine-plugin/1.1.1/pages/Templating/configuration_files/default_step_implementation.html
+// https://jenkinsci.github.io/templating-engine-plugin/pages/Library_Development/getting_started.html
 
 void call(){
-    println "gradle: build()"
+    node{
+        sh "Hello from sync step!"
+    }
 }
 
 
